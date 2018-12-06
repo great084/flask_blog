b50666463eee197e4d6db28599d07e20925a3d56
hiromichi <great084@gmail.com>
2018-12-06 02:46:23 +0000

    new entry of blog

--- views/entries.py
+++ views/entries.py
@@ -1,5 +1,6 @@
-from flask_blog import app
+from flask_blog import app, db
 from flask import request, redirect, url_for, render_template, flash, session
+from flask_blog.models.entries import Entry
 
 @app.route("/")
 def show_entries():
@@ -8,3 +9,25 @@ def show_entries():
         return redirect(url_for('login'))
         #return render_template('login.html')
     return render_template('entries/index.html')
+
+@app.route("/entries/new", methods=["GET"])
+def new_entry():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('entries/new.html')
+
+@app.route("/entries", methods=["POST"])
+def add_entry():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry(
+        title = request.form['title'],
+        text = request.form['text']
+    )
+    print(entry)
+    #db.session.add(entry)
+    print(db.session.add(entry))
+    db.session.commit()
+    flash('新しい記事が投稿されました')
+    return redirect(url_for('show_entries'))
+
